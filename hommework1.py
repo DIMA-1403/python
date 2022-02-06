fceb8827a17d1b301ff1dce61356c29b61b22c18
DIMA-1403 <97698468+DIMA-1403@users.noreply.github.com>
2022-02-06 11:12:39 +0200

    homework1
    homework1

---
+++ hommework1.py
@@ -0,0 +1,38 @@
+# task 2
+print('Hi! My name is Dima. Yow was your day today?')
+step='/n'
+
+# task 3
+
+l=4
+w=2
+s=l*w
+print('s=',s)
+
+# task 4
+
+a=int(input('a='))
+b=int(input('b='))
+
+s=a+b
+d=a-b
+p=a*b
+q=a/b
+
+print('s=',s)
+print('d=',d)
+print('p=',p)
+print('q=',q)
+
+# task 5
+
+r=int(input('r='))
+p=3.14159
+
+d=2*r
+c=2*p*r
+s=p*r**2
+
+print('d=',d)
+print('c=',c)
+print('s=',s)
